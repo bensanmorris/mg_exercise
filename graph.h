7c127dda6a408bf2fd48a0d960515de4a0d0d4cd
Ben Morris <bensan.morris@googlemail.com>
2024-10-18 13:15:20 +0100

    Added Edge + test

--- graph.h
+++ graph.h
@@ -16,13 +16,33 @@ namespace mg
         std::vector<Vertex> data;
     };
 
+    struct Edge
+    {
+        std::size_t from;
+        std::size_t to;
+
+        Edge(std::size_t from_, std::size_t to_) : from(from_), to(to_)
+        {}
+    };
+
+    struct Edges
+    {
+        std::vector<Edge> data;
+    };
+
     struct Graph
     {
         Vertices vertices;
+        Edges    edges;
 
         Vertex CreateVertex()
         {
             return vertices.data.emplace_back(vertices.data.size());
+        }
+
+        Edge CreateEdge(Vertex a, Vertex b)
+        {
+            return edges.data.emplace_back(Edge{a.id,b.id});
         }
     };
 }
--- test.cpp
+++ test.cpp
@@ -10,3 +10,17 @@ TEST(GraphTest, CreateVertexTest)
     EXPECT_EQ(g.vertices.data.size(),1);
     EXPECT_EQ(v.id,0);
 }
+
+TEST(GraphTest, CreateEdgeTest)
+{
+    Graph g;
+    auto vA = g.CreateVertex();
+    auto vB = g.CreateVertex();
+    EXPECT_EQ(g.vertices.data.size(),2);
+    EXPECT_EQ(vA.id,0);
+    EXPECT_EQ(vB.id,1);
+    auto e = g.CreateEdge(vA,vB);
+    EXPECT_EQ(g.edges.data.size(),1);
+    EXPECT_EQ(e.from, vA.id);
+    EXPECT_EQ(e.to,vB.id);
+}
